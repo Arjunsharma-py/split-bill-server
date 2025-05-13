c9a079eab414f9393dc9cd93ebdde58d0ce7acf5
Aman Hussain <amanhussain10000@gmail.com>
2025-05-13 09:51:28 +0530

    feat: project setup

---
+++ src/main/java/com/Split/SplitBillsServer/SplitBillsServerApplication.java
@@ -0,0 +1,13 @@
+package com.Split.SplitBillsServer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SplitBillsServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SplitBillsServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/Split/SplitBillsServer/SplitBillsServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Split.SplitBillsServer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SplitBillsServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
